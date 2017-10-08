57f3154524214e391923604e48cc95109620d3a5
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2017-10-08 16:00:30 +0300

    Updated configs

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,5 @@
 (function () {
-  $.get("http://localhost:9000/badges?receiverLogin=IlyaZinkovich", function (data) {
+  $.get("/badges?receiverLogin=IlyaZinkovich", function (data) {
     console.log(data);
     data.forEach(function(badge) {
       $("#badges").append("<p>" + JSON.stringify(badge) + "</p>")
