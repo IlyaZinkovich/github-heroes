ebf7b368660c937dbc9419c706a2bd8619ae457a
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2017-10-08 20:42:09 +0300

    Added search form

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,17 @@
 (function () {
-  $.get("/badges?receiverLogin=IlyaZinkovich", function (data) {
-    console.log(data);
-    data.forEach(function(badge) {
-      var badge =
-        `<div class="one-fifth column badge">
-          <img class="avatar" width="72" height="72" src="${badge.from.avatar_url}">
-          <h3>${badge.name}</h3>
-        </div>`;
-      $("#badges").append(badge);
-    });
+  $("#search-badges-button").click(function() {
+    $("#search-badges").empty();
+    var receiverLogin = $("#github-search-login").val();
+    $.get(`/badges?receiverLogin=${receiverLogin}`, function (data) {
+        console.log(data);
+        data.forEach(function(badge) {
+          var badge =
+            `<div class="one-fifth column badge">
+              <img class="avatar" width="72" height="72" src="${badge.from.avatar_url}">
+              <h3>${badge.name}</h3>
+            </div>`;
+          $("#search-badges").append(badge);
+        });
+      });
   });
 }());
